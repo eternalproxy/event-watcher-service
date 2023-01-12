6751ddb24d213ab9570ccbe57cfc85023a4de796
omnus <omnusunmo@gmail.com>
2023-01-12 19:15:42 +1300

    Update rerun.js

--- rerun.js
+++ rerun.js
@@ -103,8 +103,8 @@ async function main() {
     }
   }
   console.log(`Succesfully checked ${count} documents`)
-  console.log('Waiting 30 seconds before new checks')
-  setTimeout(function () { console.log('Reestarting checks'); main(); }, 30000);
+  console.log('Waiting 60 seconds before new checks')
+  setTimeout(function () { console.log('Reestarting checks'); main(); }, 60000);
 
 }
 
